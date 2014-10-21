6aa78b8692f0e9b5bed53011686681cba69538a4
guerot_a <guerot_a@epitech.net>
2014-10-21 18:39:23 +0200

    Ajout des header a partir de l'uml (incomplet)

---
+++ server/include/TcpAsyncServer.hh
@@ -0,0 +1,20 @@
+//
+// TcpAsyncServer.hh for  in /home/anto/workspace/git/babel/server
+//
+// Made by anto
+// Login   <anto@epitech.net>
+//
+// Started on  Tue Oct 21 18:14:28 2014 anto
+// Last update Tue Oct 21 18:15:22 2014 anto
+//
+
+#ifndef TCPASYNCSERVER_HH
+#define TCPASYNCSERVER_HH
+
+class TcpAsyncServer
+{
+public:
+  virtual void run();
+};
+
+#endif /* TCPASYNCSERVER_HH */
