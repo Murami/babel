60d66f717fcf8086cbba74d6c0590ac714a63e74
guerot_a <guerot_a@epitech.net>
2014-10-29 16:35:58 +0100

    NON COMPLETE

--- server/include/BoostTcpAsyncServer.hh
+++ server/include/BoostTcpAsyncServer.hh
@@ -8,13 +8,15 @@ class BoostTcpAsyncServer : public TcpAsyncServer
 {
 private:
   boost::asio::ip::tcp::acceptor	m_acceptor;
+  boost::asio::deadline_timer		m_timer;
 
 public:
-  BoostTcpAsyncServer(BoostAsyncService& service);
-  virtual ~BoostTcpAsyncServer();
+  BoostTcpAsyncServer(BoostAsyncService& service, int port);
+  virtual ~BoostTcpAsyncServer() {};
 
   void		accept();
-  void		setTimeout(unsigned int second, unsigned int millisecond);
+  void		wait(unsigned int second, unsigned int millisecond);
+  void		waitUntil(unsigned int second, unsigned int millisecond) = 0;
 };
 
 #endif /* BOOSTTCPASYNCSERVER_HPP */
--- server/include/ITcpAsyncServer.hh
+++ server/include/ITcpAsyncServer.hh
@@ -13,7 +13,8 @@ public:
   virtual void			deleteListener(ITcpAsyncServerListener* listener) = 0;
 
   virtual void			accept() = 0;
-  virtual void			setTimeout(unsigned int second, unsigned int millisecond) = 0;
+  virtual void			wait(unsigned int second, unsigned int millisecond) = 0;
+  virtual void			waitUntil(unsigned int second, unsigned int millisecond) = 0;
 
   virtual void			notifyAccept(ITcpAsyncClient* client) = 0;
   virtual void			notifyTimeout() = 0;
