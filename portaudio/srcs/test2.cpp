6429f7b23239b19e12301eec268b1b840d01228f
desabr_q <desabr_q@epitech.eu>
2014-11-01 18:00:44 +0100

    fuck off

---
+++ portaudio/srcs/test2.cpp
@@ -0,0 +1,19 @@
+#include "portaudio.h"
+#include <stdio.h>
+
+int	main()
+{
+  int numDevices;
+  numDevices = Pa_GetDeviceCount();
+  if( numDevices < 0 )
+    {
+      printf( "ERROR: Pa_CountDevices returned %d\n", numDevices );
+      // err = numDevices;
+    }
+  const PaDeviceInfo *deviceInfo;
+  for(int  i=0; i<numDevices; i++ )
+    {
+      deviceInfo = Pa_GetDeviceInfo( i );
+    }
+
+}
