222849c0758cc842230cae4b1db63b8dbdc60701
pinon <pinon_a@epitech.eu>
2014-11-03 18:15:51 +0100

    client functor + update widget observer

--- client/babel.pro
+++ client/babel.pro
@@ -6,10 +6,14 @@ HEADERS += 	include/QTcpAsyncSocket.hh 	\
 		include/BabelCoreClient.hh 	\
 		include/IAsyncSocketListener.hh \
                 include/ICoreListener.hh        \
-                include/IWidgetListener.hh
+                include/IWidgetListener.hh      \
+                include/IFunctor.hh             \
+                include/Functor.hh              \
+                include/Protocol.hh
 
 SOURCES += 	src/main.cpp 			\
 		src/QTcpAsyncSocket.cpp 	\
-		src/BabelCoreClient.cpp
+		src/BabelCoreClient.cpp         \
+                src/Functor.cpp
 
 OBJECTS_DIR	=	src/
---
+++ client/include/Functor.hh
@@ -0,0 +1,96 @@
+#ifndef FUNCTOR_HH
+#define FUNCTOR_HH
+
+#include "IFunctor.hh"
+
+class HeaderFunctor : public IFunctor
+{
+public:
+  void operator()(BabelCoreClient & core, void *data);
+};
+
+class UserInfoFunctor : public IFunctor
+{
+public:
+  void operator()(BabelCoreClient & core, void *data);
+};
+
+class RecvMsgFunctor : public IFunctor
+{
+public:
+  void operator()(BabelCoreClient & core, void *data);
+};
+
+class CallFunctor : public IFunctor
+{
+public:
+  void operator()(BabelCoreClient & core, void *data);
+};
+
+class OkLoginFunctor : public IFunctor
+{
+public:
+  void operator()(BabelCoreClient & core, void *data);
+};
+
+class KoLoginFunctor : public IFunctor
+{
+public:
+  void operator()(BabelCoreClient & core, void *data);
+};
+
+class OkRegisterFunctor : public IFunctor
+{
+public:
+  void operator()(BabelCoreClient & core, void *data);
+};
+
+class KoRegisterFunctor : public IFunctor
+{
+public:
+  void operator()(BabelCoreClient & core, void *data);
+};
+
+class OkCallFunctor : public IFunctor
+{
+public:
+  void operator()(BabelCoreClient & core, void *data);
+};
+
+class KoCallFunctor : public IFunctor
+{
+public:
+  void operator()(BabelCoreClient & core, void *data);
+};
+
+class OkMsgFunctor : public IFunctor
+{
+public:
+  void operator()(BabelCoreClient & core, void *data);
+};
+
+class KoMsgFunctor : public IFunctor
+{
+public:
+  void operator()(BabelCoreClient & core, void *data);
+};
+
+class SampleFunctor : public IFunctor
+{
+public:
+  void operator()(BabelCoreClient & core, void *data);
+};
+
+class ImgFunctor : public IFunctor
+{
+public:
+  void operator()(BabelCoreClient & core, void *data);
+};
+
+class PingFunctor : public IFunctor
+{
+public:
+  void operator()(BabelCoreClient & core, void *data);
+};
+
+#endif
