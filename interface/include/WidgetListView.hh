fb89363f7eca263d6d799e2a896ec6375baad46b
Emmanuel BONIN <emmanuel.bonin@epitech.eu>
2014-11-03 17:31:37 +0100

    .

--- interface/include/WidgetListView.hh
+++ interface/include/WidgetListView.hh
@@ -1,11 +1,18 @@
 #ifndef		__CONTACTLISTVIEW_HH__
 # define	__CONTACTLISTVIEW_HH__
 
+# include	<QString>
+# include	<QList>
+# include	<QStandardItemModel>
 # include	<QListView>
 # include	"IUserInfoListener.hh"
 
 class		WidgetListView : public QListView, public IUserInfoListener
 {
+private:
+  QStandardItemModel	*_model;
+  QList<QStandardItem*>	_contactList;
+
 public:
   virtual void	onData(NET::UserInfo);
 
--- interface/src/WidgetListView.cpp
+++ interface/src/WidgetListView.cpp
@@ -3,6 +3,11 @@
 
 WidgetListView::WidgetListView(QWidget *parent) : QListView(parent)
 {
+  this->_model = new QStandardItemModel();
+  // Methode pour ajouter du contenu dans la liste
+  this->_model->appendRow(new QStandardItem(QString("trololo")));
+  this->_model->appendRow(new QStandardItem(QString("coucou")));
+  this->setModel(this->_model);
 }
 
 void		WidgetListView::onData(NET::UserInfo info)
