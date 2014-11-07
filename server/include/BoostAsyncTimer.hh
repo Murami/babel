900d4332658a179c151fe1a76d6eed10dcf1df18
guerot_a <guerot_a@gmail.com>
2014-11-07 13:48:37 +0100

    [ANTO] Server : Re-Implementation des timers boost (A tester)

--- server/include/BoostAsyncTimer.hh
+++ server/include/BoostAsyncTimer.hh
@@ -8,17 +8,17 @@ class BoostAsyncService;
 
 class BoostAsyncTimer : public AsyncTimer
 {
+private:
+  boost::asio::deadline_timer	m_timer;
+
 public:
   BoostAsyncTimer(BoostAsyncService& service);
   ~BoostAsyncTimer();
 
   void	wait(unsigned int second, unsigned int microsecond);
-  void	waitUntil(unsigned int second, unsigned int microsecond);
+  // void	waitUntil(unsigned int second, unsigned int microsecond);
 
   void	onTimeout(const boost::system::error_code& e);
-
-private:
-  boost::asio::deadline_timer	m_timer;
 };
 
 #endif /* BOOSTASYNCTIMER_HH */
--- server/src/BoostAsyncTimer.cpp
+++ server/src/BoostAsyncTimer.cpp
@@ -1,7 +1,10 @@
 #include "BoostAsyncTimer.hh"
 #include "BoostAsyncService.hh"
 
-BoostAsyncTimer::BoostAsyncTimer(BoostAsyncService& service) : m_timer(service.getIOService())
+#include <boost/bind.hpp>
+
+BoostAsyncTimer::BoostAsyncTimer(BoostAsyncService& service) :
+  m_timer(service.getIOService())
 {
 }
 
@@ -11,9 +14,21 @@ BoostAsyncTimer::~BoostAsyncTimer()
 
 void	BoostAsyncTimer::wait(unsigned int second, unsigned int microsecond)
 {
+  boost::posix_time::time_duration	duration;
+
+  duration = (boost::posix_time::seconds(second) +
+	      boost::posix_time::microseconds(microsecond));
+  m_timer.expires_from_now(duration);
+  m_timer.async_wait(boost::bind(&BoostAsyncTimer::onTimeout, this,
+				 boost::asio::placeholders::error));
 }
 
-void	BoostAsyncTimer::waitUntil(unsigned int second, unsigned int microsecond)
-{
+// void	BoostAsyncTimer::waitUntil(unsigned int second, unsigned int microsecond)
+// {
 
+// }
+
+void	BoostAsyncTimer::onTimeout(const boost::system::error_code& /*e*/)
+{
+  notifyTimeout();
 }
