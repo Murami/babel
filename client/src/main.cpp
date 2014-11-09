5b2463713b14ca7476430ba71ff56adb1804d940
Emmanuel BONIN <emmanuel.bonin@epitech.eu>
2014-11-09 13:12:34 +0100

    Gestion des resources OK

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -12,13 +12,13 @@
 #include "LoginEntryDialog.hh"
 #include "RegisterEntryDialog.hh"
 
-std::string	ResourcePath::WAIT_BACKGROUND	= "./res/wait_background.jpg";
-std::string	ResourcePath::CALL_BACKGROUND	= "./res/call_background.jpg";
-std::string	ResourcePath::BUTTON_NORMAL	= "./res/button_normal.jpg";
-std::string	ResourcePath::BUTTON_HOVERED	= "./res/button_hovered.jpg";
-std::string	ResourcePath::BUTTON_PRESSED	= "./res/button_pressed.jpg";
-std::string	ResourcePath::BLUE_PELLET	= "./res/blue_pellet.png";
-std::string	ResourcePath::RED_PELLET	= "./res/red_pellet.png";
+std::string	ResourcePath::WAIT_BACKGROUND	= ":/background/wait";
+std::string	ResourcePath::CALL_BACKGROUND	= ":/background/call";
+std::string	ResourcePath::BUTTON_NORMAL	= ":/button/normal";
+std::string	ResourcePath::BUTTON_HOVERED	= ":/button/hovered";
+std::string	ResourcePath::BUTTON_PRESSED	= ":/button/pressed";
+std::string	ResourcePath::BLUE_PELLET	= ":/pellet/blue";
+std::string	ResourcePath::RED_PELLET	= ":/pellet/red";
 ResourceManager	*ResourceManager::_instance	= NULL;
 
 int main(int argc, char *argv[])
