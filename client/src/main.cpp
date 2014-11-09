0868b148141fa7cde71b05bc8a4d5adff523c0cb
Emmanuel BONIN <emmanuel.bonin@epitech.eu>
2014-11-09 10:19:01 +0100

    .

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -23,52 +23,52 @@ ResourceManager	*ResourceManager::_instance	= NULL;
 
 
 
-#include "main.hh"
+// #include "main.hh"
 
-#define PORT 4253
+// #define PORT 4253
 
-ClientReader::ClientReader(QWidget *parent) : QWidget(parent)
-{
-}
+// ClientReader::ClientReader(QWidget *parent) : QWidget(parent)
+// {
+// }
 
-void ClientReader::bind()
-{
-  QObject::connect(&this->sock, SIGNAL(stateChanged(QAbstractSocket::SocketState)), this, SLOT(setReadyRead(QAbstractSocket::SocketState)));
-  this->sock.bind(QHostAddress::Any, PORT);
-}
+// void ClientReader::bind()
+// {
+//   QObject::connect(&this->sock, SIGNAL(stateChanged(QAbstractSocket::SocketState)), this, SLOT(setReadyRead(QAbstractSocket::SocketState)));
+//   this->sock.bind(QHostAddress::Any, PORT);
+// }
 
-void ClientReader::setReadyRead(QAbstractSocket::SocketState s)
-{
-  std::cout << "change state " << s << std::endl;
-  if (s == QAbstractSocket::BoundState)
-    {
-      std::cout << "connect readyRead" << std::endl;
-      QObject::connect(&this->sock, SIGNAL(readyRead()), this, SLOT(readUdp()));
-    }
-}
+// void ClientReader::setReadyRead(QAbstractSocket::SocketState s)
+// {
+//   std::cout << "change state " << s << std::endl;
+//   if (s == QAbstractSocket::BoundState)
+//     {
+//       std::cout << "connect readyRead" << std::endl;
+//       QObject::connect(&this->sock, SIGNAL(readyRead()), this, SLOT(readUdp()));
+//     }
+// }
 
-void ClientReader::writeUdp(test *t)
-{
-  std::cout << "Writing " << t->n << std::endl;
-  sock.writeDatagram((char*)t, sizeof(*t), QHostAddress::Any, PORT);
-}
+// void ClientReader::writeUdp(test *t)
+// {
+//   std::cout << "Writing " << t->n << std::endl;
+//   sock.writeDatagram((char*)t, sizeof(*t), QHostAddress::Any, PORT);
+// }
 
-void ClientReader::readUdp()
-{
-  test t;
+// void ClientReader::readUdp()
+// {
+//   test t;
 
-  std::cout << "read : ";
-  sock.readDatagram((char*)&t, (qint64)sizeof(t));
-  std::cout << " [" << t.n << "]" << std::endl;
-}
+//   std::cout << "read : ";
+//   sock.readDatagram((char*)&t, (qint64)sizeof(t));
+//   std::cout << " [" << t.n << "]" << std::endl;
+// }
 
-#include <unistd.h>
+// #include <unistd.h>
 
-ClientReader::~ClientReader()
-{
-  std::cout << "coucuo" << std::endl;
-  this->sock.close();
-}
+// ClientReader::~ClientReader()
+// {
+//   std::cout << "coucuo" << std::endl;
+//   this->sock.close();
+// }
 
 int main(int argc, char *argv[])
 {
